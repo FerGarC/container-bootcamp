a807055f4e93519b987de07f0353bbeff8dd9d6a
FerGarC <47437231+FerGarC@users.noreply.github.com>
2019-02-07 19:41:10 -0600

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-       HOLA CRAYOLA
+       Holoooooooooo
       </div>
       <div class="col-lg-6">
       </div>
